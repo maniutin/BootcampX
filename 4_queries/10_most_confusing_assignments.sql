350192cbd172009cdfd278280938385480d27be4
Nick Maniutin <maniutin@gmail.com>
2021-03-02 20:45:45 +0000

    correct solution for most confusing assignments

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -1,4 +1,4 @@
-SELECT assignments.id, name, day, chapter, count(assistance_requests.*) as total_requests
+SELECT assignments.id, name, day, chapter, count(assistance_requests) as total_requests
 FROM assignments
 JOIN assistance_requests ON assignments.id = assignment_id
 GROUP BY assignments.id
